7d83ea712733e929664193c2547d1e41c7e345e4
monkey <gikeda@appirits.com>
2017-10-16 16:24:21 +0900

    [add] SearchViewの追加

--- app/src/main/java/com/list_sample/toolbarsamplekotlin/MainActivity.kt
+++ app/src/main/java/com/list_sample/toolbarsamplekotlin/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.list_sample.toolbarsamplekotlin
 
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.MenuItemCompat
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById(R.id.toolbar_activity_main) as android.support.v7.widget.Toolbar
+        toolbar.inflateMenu(R.menu.search)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search, menu)
+        val searchItem = menu!!.findItem(R.id.search_view)
+        return true
+    }
+
+    val listener = SearchView.OnQueryTextListene
     }
 }
