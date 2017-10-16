83708d3e0436c2cf6090d800c6057b779307c9cf
monkey <gikeda@appirits.com>
2017-10-16 18:11:14 +0900

    [add] SearchViewにListenerを追加
    SearchViewのテキストが変更されたとき
    SearchViewがSubmitされたとき
    にLogに出力する

--- app/src/main/java/com/list_sample/toolbarsamplekotlin/MainActivity.kt
+++ app/src/main/java/com/list_sample/toolbarsamplekotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.MenuItemCompat
+import android.text.TextUtils
 import android.util.Log
 import android.view.Menu
 import android.widget.SearchView
@@ -24,9 +25,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.search, menu)
         val searchItem = menu!!.findItem(R.id.search_view)
-        return true
-    }
+        val searchView = searchItem.actionView as android.support.v7.widget.SearchView
 
-    val listener = SearchView.OnQueryTextListene
+        searchView.setOnQueryTextListener(object: android.support.v7.widget.SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Log.d(TAG, "submitted text is $query")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                if (!(TextUtils.isEmpty(newText))) {
+                    Log.d(TAG, "text is changed $newText")
+                }
+                return true
+            }
+        })
+
+        return true
     }
 }
